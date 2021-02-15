ea1704b9d3db8cc1e13ebcf859c17b69d47d4441
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2021-02-15 15:30:20 +0300

    add

--- websight/src/pages/page-3.js
+++ websight/src/pages/page-3.js
@@ -9,6 +9,7 @@ const ThirdPage = () => (
     <SEO title="Page two" />
     <h1>Hi from the third page</h1>
     <p>Welcome to page 3</p>
+    <p>testing here ...</p>
     <Link to="/page-2.js">Go back to the Second Page</Link>
   </Layout>
 )
